aac2983930b26464af344981c50cd1e8c7bd0c88
Andreasonator <andreas.vinod7474@gmail.com>
2022-03-21 23:46:13 -0700

    - Using GridLayout instead of GridBagLayout for the left panel - Will make a scrollpane of buttons for the middle panel - Will use a gridbaglayout for the right panel, god help me when I do.

--- src/main/ui/DatabaseSwingUI.java
+++ src/main/ui/DatabaseSwingUI.java
@@ -45,7 +45,7 @@ public class DatabaseSwingUI extends JFrame implements ActionListener {
     private ArrayList<JButton> buttonListOfSCPs;
     private LinkedHashMap<Integer, JButton> entityButtonMap;
 
-    private GridBagLayout leftLayout;
+    private GridLayout leftLayout;
 
     public DatabaseSwingUI() {
         super("SCP Database");
@@ -53,6 +53,8 @@ public class DatabaseSwingUI extends JFrame implements ActionListener {
 
         ((JPanel) getContentPane()).setBorder(new EmptyBorder(13, 13, 13, 13));
         setLayout(new BorderLayout());
+
+        leftLayout = new GridLayout(0, 1);
 
         leftPanel = new JPanel(leftLayout);
         middlePanel = new JPanel();
@@ -90,32 +92,11 @@ public class DatabaseSwingUI extends JFrame implements ActionListener {
         // this.actionPerformed(ActionEvent e) will be called.
         // You could also set a different object, if you wanted
         // a different object to respond to the button click
+
         //label = new JLabel("flag");
         //field = new JTextField(5);
-        //add(field);
-        //add(btn);
-        //add(label);
-        //leftPanel.add(entityCatalogueScrollPane);
+
         //pack();
-
-
-
-
-        /*
-        while (keepRunning) {
-            displayMenu();
-            command = input.next();
-            command = command.toLowerCase();
-
-            if (command.equals("quit")) {
-                keepRunning = false;
-            } else {
-                processCommand(command);
-            }
-
-        }
-
-         */
 
         displayMenu();
 
@@ -185,13 +166,12 @@ public class DatabaseSwingUI extends JFrame implements ActionListener {
         leftTextArea.setWrapStyleWord(true);
         leftTextArea.setEditable(false);
         leftTextArea.setBorder(null);
-        leftTextArea.setText("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \\\"clever\\\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.");
-
+        leftTextArea.setText("wauce");
         //leftText.setFont(new Font("Serif", Font.BOLD, 18));
-        leftText = new JLabel("<html>" + "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo." + "</html>");
+        //leftText = new JLabel("<html>" + "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo." + "</html>");
 
         leftPanel.add(leftTextArea);
-        leftPanel.add(testLeft);
+        //leftPanel.add(testLeft);
     }
 
     //This is the method that is called when the the JButton btn is clicked
